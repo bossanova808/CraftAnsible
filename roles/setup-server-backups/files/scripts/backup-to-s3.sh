4570af6200d3b8c2dce34d6dd87dbffbef6e67a0
bossanova808 <bossanova808@gmail.com>
2016-06-29 12:30:07 +1000

    Backups now run as root & backup Let's encrypt SSL as well .bashrc added to server setup process

--- roles/setup-server-backups/files/scripts/backup-to-s3.sh
+++ roles/setup-server-backups/files/scripts/backup-to-s3.sh
@@ -7,13 +7,13 @@ database_pass="whatever"
 current_date=`date +%Y-%m-%d--%H-%M`
 
 # export database
-mysqldump -u $database_user -p${database_pass} $database | gzip > ~/backups/dbbackup_${database}_${current_date}.sql.gz
+mysqldump -u $database_user -p${database_pass} $database | gzip > ~/backups/db_backup_${database}_${current_date}.sql.gz
 
 # zip up lets encrypt folder (Need to be root)
-gzip -r /etc/letsencrypt ~/backups/certsbackup_${current_date}.sql.gz
+tar -czf ~/backups/certs_backup_${current_date}.tar.gz /etc/letsencrypt  
 
 # remove backups older than 14 days
 find ~/backups/db* -mtime +14 -exec rm {} \;
 
 # sync to amazon
-/usr/local/bin/aws s3 sync ~/backups s3://backups.whatever.com.au --delete
+/usr/local/bin/aws s3 sync ~/backups s3://backups.whatever.com --delete
