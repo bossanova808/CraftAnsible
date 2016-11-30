8838cdb33bd030e6a64f5c617a483bf73e2e9eed
bossanova808 <bossanova808@gmail.com>
2016-12-01 10:36:24 +1100

    Latest fixes and additions.

--- roles/setup-server-backups/files/scripts/backup-to-s3.sh
+++ roles/setup-server-backups/files/scripts/backup-to-s3.sh
@@ -12,8 +12,11 @@ mysqldump -u $database_user -p${database_pass} $database | gzip > ~/backups/db_b
 # zip up lets encrypt folder (Need to be root)
 tar -czf ~/backups/certs_backup_${current_date}.tar.gz /etc/letsencrypt  
 
-# remove backups older than 14 days
+# remove db backups older than 14 days
 find ~/backups/db* -mtime +14 -exec rm {} \;
+
+# remove cert backups older than 14 days
+find ~/backups/certs* -mtime +14 -exec rm {} \;
 
 # sync to amazon
 /usr/local/bin/aws s3 sync ~/backups s3://backups.whatever.com --delete
